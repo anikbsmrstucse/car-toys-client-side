97e7d808b0875841a973463325d2f69dcf8b029f
Anik Islam <aanikbsmrstucse@gmail.com>
2023-06-16 05:57:34 +0600

    error page done

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,17 @@
+import Lottie from "lottie-react";
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import errorPage from '../../assets/98642-error-404.json';
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
-        <div>
-            This is error pages
+        <div className="text-center">
+           <Lottie className="w-2/5 mx-auto" animationData={errorPage} loop={true}></Lottie>
+           <button onClick={goBack} className="btn btn-error">Go Back</button>
         </div>
     );
 };
